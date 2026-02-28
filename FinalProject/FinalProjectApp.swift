ae7d88203128cc0e8623540459efc2677fa169cc
naxuca <85626372+naxuca@users.noreply.github.com>
2026-02-28 19:38:18 +0400

    Initial Commit

---
+++ FinalProject/FinalProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinalProjectApp.swift
+//  FinalProject
+//
+//  Created by user on 28.02.26.
+//
+
+import SwiftUI
+
+@main
+struct FinalProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
